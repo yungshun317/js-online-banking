ddce43ec2e7195c355c0d2ef144f20f8f0134b01
yungshun317 <seraph317@gmail.com>
2023-08-17 14:09:24 +0800

    [FEAT] Use `insertAdjacentHTML` to create DOM elements

--- script.js
+++ script.js
@@ -1,17 +1,75 @@
 'use strict';
 
 // Data
+const account1 = {
+    owner: 'Joe Chang',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+};
+
+const account2 = {
+    owner: 'Mai Shiranui',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+};
+
+const account3 = {
+    owner: 'Kula Diamond',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+};
+
+const account4 = {
+    owner: 'Morrigan Aensland',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 
 const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
 
 const btnLogin = document.querySelector('.login__btn');
 
 const inputLoginUsername = document.querySelector('.login__input--user');
 const inputLoginPin = document.querySelector('.login__input--pin');
 
+const displayMovements = function(movements, sort = false) {
+    containerMovements.innerHTML = '';
+
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+    movs.forEach(function(mov, i) {
+       const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+       const html = `
+           <div class="movements__row">
+               <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+               <div class="movements__value">${mov}€</div>
+           </div>    
+       `;
+
+       containerMovements.insertAdjacentHTML('afterbegin', html);
+    });
+};
+
+// Functions
+const updateUI = function(acc) {
+    // Display movements
+    displayMovements(acc.movements);
+
+    // Display balance
+
+    // Display summary
+}
 
 // Event handlers
 let currentAccount;
