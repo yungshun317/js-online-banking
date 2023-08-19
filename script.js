4ee92c211563cf4c3af9947928ac821751248064
yungshun317 <seraph317@gmail.com>
2023-08-20 01:42:33 +0800

    [FEAT] Enable users to request loans

--- script.js
+++ script.js
@@ -192,12 +192,21 @@ btnLoan.addEventListener('click', function(e) {
     e.preventDefault();
 
     const amount = Number(inputLoanAmount.value);
+
+    if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+        // Add movement
+        currentAccount.movements.push(amount);
+
+        // Update UI
+        updateUI(currentAccount);
+    }
+    inputLoanAmount.value = '';
 });
 
 btnClose.addEventListener('click', function(e) {
     e.preventDefault();
 
-    if ( inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin ) {
+    if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
         const index = accounts.findIndex(
             acc => acc.username === currentAccount.username
         );
