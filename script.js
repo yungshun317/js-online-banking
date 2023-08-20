a1f723ec142185aa57242cd41e4a1682331f5b57
yungshun317 <seraph317@gmail.com>
2023-08-21 02:12:10 +0800

    [FEAT] Calculate how many days passed for each movement

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const account1 = {
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2, // %
     pin: 1111,
+    movementsDates: [
+        "2019-11-18T21:31:17.178Z",
+        "2019-12-23T07:42:02.383Z",
+        "2020-01-28T09:15:04.904Z",
+        "2020-04-01T10:17:24.185Z",
+        "2020-05-08T14:11:59.604Z",
+        "2020-07-26T17:01:17.194Z",
+        "2020-07-28T23:36:17.929Z",
+        "2020-08-01T10:51:36.790Z",
+    ],
+    currency: "EUR",
+    locale: "pt-PT",
 };
 
 const account2 = {
@@ -13,6 +25,18 @@ const account2 = {
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
+    movementsDates: [
+        "2019-11-01T13:15:33.035Z",
+        "2019-11-30T09:48:16.867Z",
+        "2019-12-25T06:04:23.907Z",
+        "2020-01-25T14:18:46.235Z",
+        "2020-02-05T16:33:06.386Z",
+        "2020-04-10T14:43:26.374Z",
+        "2020-06-25T18:49:59.371Z",
+        "2020-07-26T12:01:20.894Z",
+    ],
+    currency: "USD",
+    locale: "en-US",
 };
 
 const account3 = {
@@ -56,18 +80,37 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 // Functions
-const displayMovements = function(movements, sort = false) {
+const formatMovementDate = function(date, locale) {
+    const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+    const daysPassed = calcDaysPassed(new Date(), date);
+    console.log(daysPassed);
+
+    if (daysPassed === 0) return "Today";
+    if (daysPassed === 1) return "Yesterday";
+    if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+    return new Intl.DateTimeFormat(locale).format(date);
+};
+
+const displayMovements = function(acc, sort = false) {
     containerMovements.innerHTML = '';
 
-    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+    const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
 
     movs.forEach(function(mov, i) {
        const type = mov > 0 ? 'deposit' : 'withdrawal';
 
+       const date = new Date(acc.movementsDates[i]);
+       const displayDate = formatMovementDate(date, acc.locale);
+
+       const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
        const html = `
            <div class="movements__row">
                <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-               <div class="movements__value">${mov}€</div>
+               <div class="movements__date">${displayDate}</div>
+               <div class="movements__value">${formattedMov}</div>
            </div>    
        `;
 
@@ -125,7 +168,7 @@ createUsernames(accounts);
 
 const updateUI = function(acc) {
     // Display movements
-    displayMovements(acc.movements);
+    displayMovements(acc);
 
     // Display balance
     calcDisplayBalance(acc);
@@ -225,6 +268,6 @@ btnClose.addEventListener('click', function(e) {
 let sorted = false;
 btnSort.addEventListener('click', function(e) {
    e.preventDefault();
-   displayMovements(currentAccount.movements, !sorted);
+   displayMovements(currentAccount, !sorted);
    sorted = !sorted;
 });
