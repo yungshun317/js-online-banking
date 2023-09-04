e8698dd6958386784fc90e737e26b775fea9b591
yungshun317 <seraph317@gmail.com>
2023-09-04 15:06:02 +0800

    [FEAT] Add an event listener to the common parent element and then determine what element originates the event for page navigation

--- script.js
+++ script.js
@@ -69,6 +69,27 @@ btnScrollTo.addEventListener('click', function (e) {
     section1.scrollIntoView({ behavior: 'smooth' });
 })
 
+// Page navigation
+/*
+document.querySelectorAll('.nav__link').forEach(function (el) {
+   el.addEventListener('click', function(e) {
+      e.preventDefault();
+      const id = this.getAttribute('href');
+      console.log(id);
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+   });
+});
+ */
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Matching strategy
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+})
+
 // [2] Online banking
 // Data
 const account1 = {
