5b933e902456437aa9a0f1cd4907de2e23d961b9
yungshun317 <seraph317@gmail.com>
2023-09-12 16:33:36 +0800

    [FEAT] Implement sticky navigation using `IntersectionObserver`

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
 
 // Modal window
 const openModal = function (e) {
@@ -129,6 +130,25 @@ const handleHover = function (e) {
 // Pass arguments into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+    const [entry] = entries;
+    console.log(entry);
+
+    if (!entry.isIntersecting) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
 
 // [2] Online banking
 // Data
