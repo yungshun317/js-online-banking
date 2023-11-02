73d8eddabe89fffe7cbc20ba61e8f43826b5f5e4
yungshun317 <seraph317@gmail.com>
2023-11-02 17:22:29 +0800

    [FEAT] Use data attributes and `IntersectionObserver` to implement image lazy loading

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
 const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
+const imgTargets = document.querySelectorAll('img[data-src]');
 
 // Modal window
 const openModal = function (e) {
@@ -171,6 +172,30 @@ allSections.forEach(function (section) {
     sectionObserver.observe(section);
     section.classList.add('section--hidden');
 })
+
+// Lazy Loading
+const loadImg = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    // Replace `src` with `data-src`
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener("load", function () {
+        entry.target.classList.remove('lazy-img');
+    })
+
+    observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px'
+})
+
+imgTargets.forEach(img => imgObserver.observe(img));
 
 // [2] Online banking
 // Data
